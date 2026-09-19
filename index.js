8f71704c494af45ed5dce5fa92c6bda6f944d879
Yuliya Platsitsyna <yuliya.platsitsyna@gmail.com>
2026-09-19 23:20:54 +0200

    add jwt

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   response.send("Hello World!");
+});
+
+app.post("/auth/login", (request, response) => {
+  const token = jwt.sign(
+    { email: request.body.email, fullName: "John Doe" },
+    "secret",
+    {
+      expiresIn: "1h",
+    }
+  );
+
+  response.json({ success: true, token });
 });
 
 app.listen(4444, (err) => {
